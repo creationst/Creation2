0d5a9df3e96e4f7febf272fd90b167257acf360c
Marcos <marcosalvare@gmail.com>
2016-07-28 19:13:48 +0100

    Changed Abaout with different EULA

--- Sources/AboutViewController.m
+++ Sources/AboutViewController.m
@@ -48,13 +48,7 @@
      
      "GNU General Public License Version 2: https://opensource.org/licenses/GPL-2.0 \n\n"
      
-     "LICENSED APPLICATION END USER LICENSE AGREEMENT (EULA): Is under MIT License: \n\n"
-     
-     "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\n"
-     
-     "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\n"
-     
-     "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."];
+     "LICENSED APPLICATION END USER LICENSE AGREEMENT (EULA): Bi-licensed under MPLv2 and GPLv2.1"];
     
     [self.view addSubview:text];
     
